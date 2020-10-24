c8840891b8ab16aa9f4fac83ff390fd09fc2c3c1
josephptflanagan <josephptflanagan@gmail.com>
2020-10-24 15:53:17 -0500

    local commit pre pull

--- client/src/components/Player/Player.js
+++ client/src/components/Player/Player.js
@@ -4,14 +4,15 @@ import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import './Player.css';
 
- 
+let source = 'http://d28dtfvuvlqgls.cloudfront.net/buskr-audio/instrumental-chill_-_Rain.mp3' 
+
 const Player = () => (
   <AudioPlayer
     autoPlay
     showJumpControls={false}
     showDownloadProgress={false}
     showSkipControls={false}
-    // src="http://example.com/audio.mp3"
+    src={source}
     onPlay={e => console.log("onPlay")}
     // other props here
   />
