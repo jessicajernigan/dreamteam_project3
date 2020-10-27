c23e600709245eab69cededc0261541c1fcadc08
geocode-matt <mlindenx@gmail.com>
2020-10-26 20:58:10 -0500

    working on cart modal

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button';
 import { BiTrash } from 'react-icons/bi';
@@ -6,17 +6,51 @@ import { BiTrash } from 'react-icons/bi';
 import './Cart.css'
 
 function Cart() {
-	const [show, setShow] = useState(false);
+// 	const [show, setShow] = useState(false);
 
-  	const handleClose = () => setShow(false);
-  	const handleShow = () => setShow(true);
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+	const node = useRef()
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleClick = e => {
+		if (node.current.contains(e.target)) {
+		  // inside click
+		  return;
+		}
+		// outside click 
+		setIsOpen(!isOpen);
+	  };
+
 	return (
 		<>
-		<a className="ViewCartBtn" onClick={handleShow}>
+		<a className="ViewCartBtn" onClick={() => setIsOpen(!isOpen)}>
 			View Cart
 		</a>
 
-		<Modal className="ViewCartModal" tabindex="0" show={show} onHide={handleClose} animation={false} backdrop={false} scrollable={false}>
+		{isOpen ? (
+			<div className="Cart" ref={node}>
+				<div className="modal-header">
+					<h4 className="">View Cart</h4>
+					<span className="close-modal-btn" onClick={() => setIsOpen(!isOpen)}>x</span>
+				</div>
+				<div className="modal-content">
+					<div className="cart-contents">
+						<p className="cart-item m-1"><Button className="RemoveFromCart p-0"><BiTrash className="m-1"/></Button> $0.99 - Red Hot Chili Peppers - Californication</p>
+						<p className="cart-item m-1"><Button className="RemoveFromCart p-0"><BiTrash className="m-1"/></Button> $0.99 - Red Hot Chili Peppers - Californication</p>
+						<p className="cart-item m-1"><Button className="RemoveFromCart p-0"><BiTrash className="m-1"/></Button> $0.99 - Red Hot Chili Peppers - Californication</p>
+				<h5 className="text-right m-2">Total: $2.97</h5>
+					</div>
+					<div className="modal-footer">
+						<Button className="CheckOutBtn btn-sm" onClick={() => setIsOpen(!isOpen)}>Check Out</Button>
+					</div>
+				</div>
+    		</div>
+		) : null}
+    
+
+		{/* <Modal className="ViewCartModal" show={show} onHide={handleClose} animation={false} backdrop={false} scrollable={false}>
 			<Modal.Header closeButton>
 			<Modal.Title>View Cart</Modal.Title>
 			</Modal.Header>
@@ -31,7 +65,7 @@ function Cart() {
 				Check Out
 			</Button>
 			</Modal.Footer>
-		</Modal>
+		</Modal> */}
     	</>
 	);
   }
