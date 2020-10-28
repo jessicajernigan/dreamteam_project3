fd93cc556474802213718d429be31356b8ac02f5
geocode-matt <mlindenx@gmail.com>
2020-10-28 16:27:38 -0500

    fixed tile images (i think)

--- client/src/components/CreatrTile/CreatrTile.js
+++ client/src/components/CreatrTile/CreatrTile.js
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom';
 import './CreatrTile.css';
 
 const CreatrTile = ({ _id, stageName, imgUrl }) => {
+	let tileStyle = {
+		backgroundImage: "url(" + imgUrl + ")",
+		position: 'relative',
+    	backgroundRepeat: 'no-repeat',
+    	backgroundSize: 'cover',
+    	backgroundPosition: 'center'
+	};
 	return (
 		<Link to={`/profile/${_id}`} className="CreatrTile">
 			<div className="CreatrTile-tile">
 				{/* <img src="https://via.placeholder.com/300" alt="" /> */}
-				<img className="Creatr-image" src={imgUrl} alt="" />
+				<img className="Creatr-image" style={tileStyle} alt="" />
         <h3>{stageName}</h3>
 			</div>
 		</Link>
