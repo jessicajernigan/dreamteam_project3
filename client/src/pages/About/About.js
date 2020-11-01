ea6785f444d2ddcef88a7f2d9b22c79db23617d7
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-11-01 07:37:03 -0600

    About page structure in place; adding content

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './About.css';
+
+const About = () => {
+	return (
+		<React.Fragment>
+			<main className="vh-100 d-flex flex-column align-items-center mt-5 pt-5">
+				<h3 className="mb-5">About Us</h3>
+				<p>Testing text here for the about me. Lorem ipsum. Dim sum. Mmmmm.</p>
+			</main>
+		</React.Fragment>
+	);
+};
+
+export default About;
